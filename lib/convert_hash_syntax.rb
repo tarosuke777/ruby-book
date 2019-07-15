14d3d5680d03c0d37561dcf2301ac39af3148261
tarosuke <felis.silvestris.catus.7777@gmail.com>
2019-07-15 19:29:05 +0900

    Revert "test"
    This reverts commit ae78efaac4a22c6ef81fcb7695aa3a9775db2d6a.

--- lib/convert_hash_syntax.rb
+++
@@ -1,3 +0,0 @@
-def convert_hash_syntax(old_syntax)
-  old_syntax.gsub(/:(\w+) *=> */,'\1:')
-end
--- sample_test.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-
-class SampleTest < Minitest::Test
- def test_sample
-  #assert_equal 'RUBY','ruby'.upcase
-  #assert_equal 'RUBY','ruby'.capitalize
-  assert_equal 'RUBY',nil.capitalize
- end
-end
