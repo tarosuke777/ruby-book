05e21aeb9a3b0ab7b45aa4508353048b25ad3e07
tarosuke <felis.silvestris.catus.7777@gmail.com>
2019-08-03 16:47:42 +0900

    'byebyg'

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -3,6 +3,7 @@ require '.\lib\fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
  def test_fizz_buzz
+  #require 'byebug';byebug
   assert_equal'1',fizz_buzz(1)
   assert_equal'2',fizz_buzz(2)
   assert_equal'Fizz',fizz_buzz(3)
